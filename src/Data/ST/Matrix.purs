c2acd6016a1a8c781aab77a53d315c64de4ea50a
Charles Schleich <schleich.cdaniel@gmail.com>
2018-07-16 18:48:36 +0200

    Attempting to update to 0.12, build fail

--- src/Data/ST/Matrix.purs
+++ src/Data/ST/Matrix.purs
@@ -15,7 +15,10 @@
 module Data.ST.Matrix where
 
 import Prelude
-import Control.Monad.Eff (Eff)
+-- import Effect
+
+-- import Control.Monad.Eff (Eff)
+import Effect
 import Control.Monad.ST (ST())
 import Data.TypeNat (class Sized)
 import Data.Array.ST (STArray)
@@ -26,14 +29,14 @@ newtype STMat r c h a = STMat (STArray h a)
 
 -- try array cloning with .slice() instead of the for-loop
 -- implementation in Data.Array.ST. Needs benchmarking.
-foreign import copyImpl :: forall a b h e. a -> Eff (st :: ST h | e) b
+foreign import copyImpl :: forall a b. a -> Effect b
 
 -- | Create an immutable copy of a mutable array.
-freeze :: forall a h e. STArray h a -> Eff (st :: ST h | e) (Array a)
+freeze :: forall a h e. STArray h a -> Effect (Array a)
 freeze = copyImpl
 
 -- | Create a mutable copy of an immutable array.
-thaw :: forall a h e. Array a -> Eff (st :: ST h | e) (STArray h a)
+thaw :: forall a h e. Array a -> Effect(STArray h a)
 thaw = copyImpl
 
 -- | Freeze an ST array. Do not mutate the STArray afterwards!
@@ -41,32 +44,35 @@ foreign import unsafeFreeze :: forall a h. STArray h a -> Array a
 
 foreign import unsafeThaw :: forall a h. Array a -> STArray h a
 
-cloneSTMat :: forall r c h a e. (STMat r c h a) -> Eff (st :: ST h | e) (STMat r c h a)
+cloneSTMat :: forall r c a. (STMat r c h a) -> Effect(STMat r c h a)
 cloneSTMat (STMat arr) = STMat <<< unsafeThaw <$> freeze arr
 
-fromSTMat :: forall r c h a e. Sized r => Sized c => (STMat r c h a) -> Eff (st :: ST h | e) (M.Mat r c a)
+fromSTMat :: forall r c a. Sized r => Sized c => (STMat r c h a) -> Effect (M.Mat r c a)
 fromSTMat (STMat arr) = do
     x   <- freeze arr
     pure (M.fromArrayColumns x)
 
-toSTMat :: forall r c h a e. (M.Mat r c a) -> Eff (st :: ST h | e) (STMat r c h a)
+toSTMat :: forall r c a. (M.Mat r c a) -> Effect (STMat r c h a)
 toSTMat m = STMat <$> thaw (M.toArrayColumns m)
 
 -- copyToSTMat :: forall r c h a e. (M.Matrix (M.Mat r c) a) => (M.Mat r c a) -> (STMat r c h a) -> Eff (st :: ST h | e) Unit
 
-foreign import copyToSTMat :: forall r c h a e. (M.Mat r c a) -> (STMat r c h a) -> Eff (st :: ST h | e) Unit
+foreign import copyToSTMat :: forall r c h a. (M.Mat r c a) -> (STMat r c h a) -> Effect Unit
 
-identityST' :: forall r c h e. Sized r => Sized c => Eff (st :: ST h | e) (STMat r c h Number)
+identityST' :: forall r c. Sized r => Sized c => Effect (STMat r c h Number)
 identityST' =
     let m = M.identity' :: M.Mat r c Number
     in STMat <$> thaw (M.toArrayColumns m)
 
-foreign import scaleSTMatrixInt :: forall a h e. (EuclideanRing a) => a -> STArray h a -> Eff (st :: ST h | e) Unit
+foreign import scaleSTMatrixInt :: forall h a. (EuclideanRing a) => a -> STArray h a -> Effect Unit
 
-scaleSTMatrix :: forall r c a h e. (EuclideanRing a) => a -> (STMat r c h a) -> Eff (st :: ST h | e) (STMat r c h a)
+scaleSTMatrix :: forall r c a. (EuclideanRing a) => a -> (STMat r c h a) -> Effect (STMat r c h a)
 scaleSTMatrix x v@(STMat arr) = scaleSTMatrixInt x arr *> pure v
 
-fromMatrix :: forall r c a h e. M.Mat r c a -> Eff (st :: ST h | e) (STMat r c h a)
+fromMatrix :: forall r c a. M.Mat r c a -> Effect (STMat r c h a)
 fromMatrix (M.Mat m) = STMat <$> thaw m
 
-foreign import runSTMatrix :: forall r c a e. (forall h. Eff (st :: ST h | e) (STMat r c h a)) -> Eff e (M.Mat r c a)
+foreign import runSTMatrix :: forall s a r. (forall h c. Effect (STMat r c h a)) -> Effect (M.Mat s a) 
+??? not sure above
+
+-- foreign import runSTMatrix :: forall s a r. (forall h. Eff (st :: ST h | r) (STMat s h a)) -> Eff r (M.Mat s a)
